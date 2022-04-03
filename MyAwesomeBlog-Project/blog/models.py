f39bec39c1cc0bea732e5659790e862748154590
INSIDE\Dmitry.Mishutin <mishutindima@gmail.com>
2022-04-03 19:34:04 +0300

    add blog entity

--- MyAwesomeBlog-Project/blog/models.py
+++ MyAwesomeBlog-Project/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    text = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to="post_images/")
