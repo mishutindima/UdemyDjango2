f8d576d354bdd63e95defeede17e44e2fd2e9f50
INSIDE\Dmitry.Mishutin <mishutindima@gmail.com>
2022-05-02 21:49:21 +0300

    1) specific post 2) urls in menu 3) static files

--- MyAwesomeBlog-Project/blog/urls.py
+++ MyAwesomeBlog-Project/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.showblog, name='showblog')
+    path("", views.showblog, name='showblog'),
+    path("<int:post_id>/", views.specific_post, name="specific_post"),
 ]
